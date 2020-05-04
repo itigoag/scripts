e3c907866c0bbd9264acb81da9f3427ea7432e52
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2020-05-05 00:25:13 +0200

    inital commit

---
+++ choco/Choco-Update.ps1
@@ -0,0 +1,20 @@
+$choco_sucees = @()
+$choco_error = @()
+$healthchecks_config = Get-Content -Raw -Path C:\itigo\config\healthchecks.json | ConvertFrom-Json
+Invoke-WebRequest -Uri "$($healthchecks_config.HEALTHCHECK_URL)/ping/$($healthchecks_config.HEALTCHECKS_CHOCO_API_KEY)/start" -Method POST 
+choco list --local-only --limit-output | ConvertFrom-Csv -Delimiter '|' -Header 'Name' | ForEach-Object {
+    choco upgrade -y $_.Name
+    if ($LASTEXITCODE -eq 0) {
+        $choco_success += $_.Name
+    }
+    else {
+        $choco_error += $_.Name
+    }
+}
+
+if ($choco_error -gt 0) {
+    Invoke-WebRequest -Uri "$($healthchecks_config.HEALTHCHECK_URL)/ping/$($healthchecks_config.HEALTCHECKS_CHOCO_API_KEY)/fail" -Method POST -Body "Error: $($choco_error -join ',') Success: $($choco_sucees -join ', ')" 
+}
+else {
+    Invoke-WebRequest -Uri "$($healthchecks_config.HEALTHCHECK_URL)/ping/$($healthchecks_config.HEALTCHECKS_CHOCO_API_KEY)" -Method POST -Body "Success: $($choco_sucees -join ', ')" 
+}
